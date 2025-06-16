56fbc1d6f4bc5916c283b343484cacba1a02d4fc
Gleason <bianliuzhu@gmail.com>
2025-06-16 11:16:00 +0800

    更新mock插件

--- debug.ts
+++ debug.ts
@@ -1,10 +1,14 @@
 import { startServer } from "./src/server.js";
 import path from "path";
 import { getDirname } from "./src/util.js";
-import { fileURLToPath } from "url";
 
 // 启动开发服务器
 startServer({
-  path: path.join(getDirname(import.meta.url), "../routes-data"), // 使用 path.join 构建正确的相对路径
+  path: path.join(
+    getDirname(import.meta.url),
+    "../../../UMF/game-creation-platform/mocks"
+    // "../routes-data"
+    // "../mocks"
+  ), // 使用 path.join 构建正确的相对路径
   port: 3720, // 开发服务器端口
 });
